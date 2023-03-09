b7b51f2354bc5904b11f309536d3c24837c4a58a
Aarti Sangwan <asangwan@worcesterk12.org>
2023-03-09 09:08:43 -0500

    Initial Commit

---
+++ Project/ProjectApp.swift
@@ -0,0 +1,17 @@
+//
+//  ProjectApp.swift
+//  Project
+//
+//  Created by Aarti Sangwan on 3/9/23.
+//
+
+import SwiftUI
+
+@main
+struct ProjectApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
